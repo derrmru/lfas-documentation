94a1f4b1f5fbd8cafa9f25ce3e67f477c1b452f5
Peter Sweeney <peter.sweeney@insightsoftware.com>
2022-03-06 20:01:43 +0100

    logo as code

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,13 @@
-import Image from 'next/image'
+import Logo from './Logo'
 import Link from 'next/link'
 
 const Header = () => {
     return (
         <header className="w-full text-center p-4 border-b-2 fixed top-0 flex justify-between items-center bg-white">
             <Link href='/' passHref>
-                <Image
-                    src="/images/LFAS_logo.svg"
-                    alt="Picture of the author"
-                    width={70}
-                    height={70}
-                    className='cursor-pointer'
-                />
+                <div className="w-14 cursor-pointer">
+                    <Logo />
+                </div>
             </Link>
 
         </header>
